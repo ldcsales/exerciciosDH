442f34580ffd673976c5bfc3c6a4d903773120aa
Lucas Sales <lucas.gol@uol.com.br>
2021-07-30 21:48:58 -0300

    Criado classe Functions

---
+++ src/main/kotlin/Functions.kt
@@ -0,0 +1,23 @@
+/*fun main(){
+    print(umMaisUm())
+}
+
+fun umMaisUm() : Int{
+    return 1+1
+}*/
+
+fun main(){
+    println("Hello World!")
+    println(soma(5,10))
+}
+
+fun soma(numero1: Int,numero2: Int) : Int{
+    return numero1 + numero2
+}
+
+/*
+Exercicios casa
+https://kotlinlang.org/docs/basic-syntax.html
+
+
+ */
