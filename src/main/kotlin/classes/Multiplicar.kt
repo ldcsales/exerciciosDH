20a55d966a18c4fcb089a6a3ace5b058f0e73fb3
Lucas Sales <lucas.gol@uol.com.br>
2021-08-02 22:30:32 -0300

    Realizado a documentacao do codigo

--- src/main/kotlin/classes/Multiplicar.kt
+++ src/main/kotlin/classes/Multiplicar.kt
@@ -1,6 +1,16 @@
 package classes
 
+/*
+Escreva uma classe que receba uma lista de números
+e uma função que imprime no console o produto
+ou seja o resultado de multiplicar todos os elementos entre si.
+Por exemplo: array[1, 4, 7] deve imprimir 28, que é 1 * 4 * 7.
+ */
+
 class Multiplicar(var lista: List<Int> ) {
+
+    //Multiplica os nmumeros do array
+
     fun multiplicaArray() : Int{
         var i = 0
         var resultado = lista[i]
@@ -11,6 +21,9 @@ class Multiplicar(var lista: List<Int> ) {
         println(resultado)
         return resultado
     }
+
+    //Funcao printar o resultado da funcao multiplicaArray
+
     fun printarResultado(){
         println(multiplicaArray())
     }
