56555f62d37b2cfc89f1e6633318dc75c9b296e0
Lucas Sales <lucas.gol@uol.com.br>
2021-08-02 22:00:58 -0300

    Criado classe aposentado e funcao de verificacao

--- src/main/kotlin/classes/Main.kt
+++ src/main/kotlin/classes/Main.kt
@@ -5,4 +5,9 @@ fun main(){
     val aluno = Aluno("Lucas Sales", 27)
 
     println(aluno.nome + " " + aluno.idade + " " + aluno.curso + " " + aluno.matricula)
+
+
+    val aposentado = Aposentado(65,'M',30).aposentadoria()
+    println(aposentado)
+
 }
---
+++ src/main/kotlin/classes/Pessoa.kt
@@ -0,0 +1,33 @@
+package classes
+
+/*Escreva uma classe que receba como parâmetro uma
+idade,
+um sexo e
+os anos   de   contribuição   para   a   aposentadoria
+e
+desenvolva   uma   função   que imprime no console verdadeiro ou falso.
+Lembrando que: - A idade mínima para mulheres é de 60 anos; -
+A idade mínima para homens é de 65 anos; -
+Em ambos os casos as pessoas devem ter pelo menos 30 anos de contribuição.
+
+ */
+
+class Aposentado(var idade: Int, var sexo: Char,var anosContribuicao: Int) {
+
+    //var sexoU = sexo.to
+
+    fun aposentadoria(): Boolean {
+        if ((sexo == 'm') || (sexo == 'M')) {
+            if ((idade >= 65) && (anosContribuicao >= 30)) {
+                return true
+            }
+        } else if ((sexo == 'f') || (sexo == 'F')) {
+            if ((idade >= 60) && (anosContribuicao >= 30)) {
+                return true
+            }
+        }
+        return false
+    }
+}
+
+
