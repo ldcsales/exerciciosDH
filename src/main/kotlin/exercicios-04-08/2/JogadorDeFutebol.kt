b0d76dc2777d6f35f4c38079366c4c38b8234ce7
Lucas Sales <lucas.gol@uol.com.br>
2021-08-04 20:48:47 -0300

    Criado funcao fazerGOl

--- src/main/kotlin/exercicios-04-08/2/JogadorDeFutebol.kt
+++ src/main/kotlin/exercicios-04-08/2/JogadorDeFutebol.kt
@@ -2,6 +2,11 @@ package `exercicios-04-08`.`2`
 
 class JogadorDeFutebol(var nome : String , var energia : Int, var alegria  : Int, var gols: Int, experiencia : Int) {
 
-
+    fun fazerGol(){
+        energia = energia - 5
+        alegria = alegria + 10
+        gols = gols + 1
+        println("GOOOOOOL")
+    }
 
 }
