b0c7e405dcc58e18d4b6a47401198e97d85f9160
Lucas Sales <lucas.gol@uol.com.br>
2021-08-04 20:40:38 -0300

    Criado funcao deposito

--- src/main/kotlin/exercicios-04-08/1/Cliente.kt
+++ src/main/kotlin/exercicios-04-08/1/Cliente.kt
@@ -1,5 +1,9 @@
 package `exercicios-04-08`.`1`
 
+/*
+1. Definir uma nova classe Cliente contendo as seguintes propriedades: - nome - sobrenome
+ */
+
 class Cliente(var nome: String, var sobrenome: String) {
 
 }
--- src/main/kotlin/exercicios-04-08/1/Conta.kt
+++ src/main/kotlin/exercicios-04-08/1/Conta.kt
@@ -1,4 +1,21 @@
 package `exercicios-04-08`.`1`
 
+/*
+2. Definir uma nova classe Conta contendo as seguintes propriedades:
+- número da conta
+- saldo
+- titular (o titular é um objeto do tipo Cliente).
+ */
+
 class Conta(var numero: Int, var saldo: Double, var titular: Cliente) {
+
+    fun deposito(valor : Double){
+        saldo = saldo + valor
+        println(imprimirSaldo())
+    }
+
+    fun imprimirSaldo(){
+        println("Saldo Atual: $saldo")
+    }
+
 }
