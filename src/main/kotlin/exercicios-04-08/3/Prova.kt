9998fd4431d6c092f3c6ea8f0844fedfcd9f2553
Lucas Sales <lucas.gol@uol.com.br>
2021-08-04 22:51:28 -0300

    Criado funcao podeRealizar

--- src/main/kotlin/exercicios-04-08/3/Prova.kt
+++ src/main/kotlin/exercicios-04-08/3/Prova.kt
@@ -2,5 +2,15 @@ package `exercicios-04-08`.`3`
 
 class Prova(var dificuldade: Int, var energiaNecessaria: Int) {
 
+    fun podeRealizar(atleta : Atleta) {
+
+        if((atleta.nivel >= dificuldade) && atleta.energia >= energiaNecessaria){
+            true
+        }else{
+            false
+        }
+    }
+
+
 
 }
