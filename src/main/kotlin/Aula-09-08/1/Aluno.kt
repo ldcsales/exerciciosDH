ee15404fae9721abb5c1c79c90d998628ea6588e
Lucas Sales <lucas.gol@uol.com.br>
2021-08-09 19:51:12 -0300

    Criado classe Aula e Aluno

--- src/main/kotlin/Aula-09-08/1/Aluno.kt
+++ src/main/kotlin/Aula-09-08/1/Aluno.kt
@@ -1,4 +1,6 @@
 package `Aula-09-08`.`1`
 
-class Aluno {
+class Aluno(var ra: Int, var nome: String, var sobrenome: String) {
+
+
 }
--- src/main/kotlin/Aula-09-08/1/Aula.kt
+++ src/main/kotlin/Aula-09-08/1/Aula.kt
@@ -1,4 +1,6 @@
 package `Aula-09-08`.`1`
 
-class Aula {
+import java.sql.Time
+
+class Aula(materia: Materia, var horarioInicio : Time, var horarioTermino: Time) {
 }
