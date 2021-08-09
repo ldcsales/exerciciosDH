1b599b3aee8b755b6dd27c3d4c83ac175c1e1837
Lucas Sales <lucas.gol@uol.com.br>
2021-08-08 23:22:59 -0300

    Criado classe Conta

--- src/main/kotlin/Santander/Conta.kt
+++ src/main/kotlin/Santander/Conta.kt
@@ -1,4 +1,27 @@
 package Santander
 
-class Conta {
+class Conta(var saldo: Double, var cliente: Cliente) {
+
+    fun deposito(valor : Double){
+        saldo += valor
+        println(imprimirSaldo())
+    }
+
+    fun imprimirSaldo() : String{
+        //println("Saldo Atual: $saldo")
+        return "Saldo Atual: $saldo"
+    }
+
+    fun saque(valor : Double){
+
+        if(saldo >= valor){
+            saldo -= valor
+            println("Transacao Realizada\n${imprimirSaldo()}")
+
+        }else{
+            println("Saldo Insuficiente")
+        }
+
+    }
+
 }
