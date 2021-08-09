4f291ab635660c03bffc2d9668efe45c47f6a6ca
Lucas Sales <lucas.gol@uol.com.br>
2021-08-09 20:54:20 -0300

    Criado funcao chamada, veridicar cont

--- src/main/kotlin/Aula-09-08/1/Professor.kt
+++ src/main/kotlin/Aula-09-08/1/Professor.kt
@@ -2,22 +2,24 @@ package `Aula-09-08`.`1`
 
 class Professor(var nome: String, var rd: Int) {
 
-    fun darAula(aula: Aula): Boolean{
+    fun darAula(aula: Aula): Boolean {
         println("Aula realizada.")
         return true
     }
 
-    fun fazerChamada(listaAlunos: List<Aluno>, aula: Aula): List<Int>{
+    fun fazerChamada(listaAlunos: List<Aluno>, aula: Aula): List<Int> {
         var listaPresenca = mutableListOf<Int>()
-        var cont = 0
-        var cont2 = 0
-        for(cont in listaAlunos){
-            for (cont2 in aula.listaPresentes){
-                if(listaAlunos[cont] == aula.listaPresentes[cont2]){
-                    listaPresenca.add(listaAlunos[cont])
+        var cont : Int = 0
+        var cont2 : Int = 0
+        for (cont in listaAlunos) {
+            for (cont2 in aula.listaPresentes) {
+                if (listaAlunos[cont] == aula.listaPresentes[cont2]) {
+                    listaPresenca.add(1)
+                    break
                 }
-            }
 
+            }
+            listaPresenca.add(0)
         }
 
         return listaPresenca
