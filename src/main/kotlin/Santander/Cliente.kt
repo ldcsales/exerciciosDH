4bfa13b8c04fe3296523543ae24d08289bea1ec5
Lucas Sales <lucas.gol@uol.com.br>
2021-08-08 23:21:20 -0300

    Criado classe Cliente

---
+++ src/main/kotlin/Santander/Cliente.kt
@@ -0,0 +1,4 @@
+package Santander
+
+class Cliente(var numeroCLiente : Int, var sobrenome: String, var rg: Int, var cpf: Int, var conta: Conta) {
+}
---
+++ src/main/kotlin/Santander/Conta.kt
@@ -0,0 +1,4 @@
+package Santander
+
+class Conta {
+}
