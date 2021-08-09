381ffa4b39a97bbba6f71c651ae3eb5e9e88c57c
Lucas Sales <lucas.gol@uol.com.br>
2021-08-09 20:12:35 -0300

    Criado funcoes Professor e Aluno.

--- src/main/kotlin/Aula-09-08/1/Aluno.kt
+++ src/main/kotlin/Aula-09-08/1/Aluno.kt
@@ -2,11 +2,15 @@ package `Aula-09-08`.`1`
 
 class Aluno(var ra: Int, var nome: String, var sobrenome: String) {
 
-    fun assistirAula(){
+    fun assistirAula(): Boolean{
+        println("Aula assistida")
+
+        return true
 
     }
 
-    fun fazerLicao(){
-
+    fun fazerLicao(): Boolean{
+        println("Licao Feita")
+        return true
     }
 }
--- src/main/kotlin/Aula-09-08/1/Professor.kt
+++ src/main/kotlin/Aula-09-08/1/Professor.kt
@@ -2,12 +2,20 @@ package `Aula-09-08`.`1`
 
 class Professor(var nome: String, var rd: Int) {
 
-    fun darAula(){
-
+    fun darAula(aula: Aula): Boolean{
+        println("Aula realizada.")
+        return true
     }
 
-    fun fazerChamada(){
+    fun fazerChamada(listaAlunos: List<Aluno>){
 
+        var cont = 0
+        var listaPresentes = mutableListOf<Aluno>()
+        for(cont in listaAlunos){
+
+        }
+
+        return
     }
 
 
