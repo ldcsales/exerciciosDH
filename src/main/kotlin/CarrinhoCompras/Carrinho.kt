e039a58408cba17bfa36689c3fe4a47a58958859
Lucas Sales <lucas.gol@uol.com.br>
2021-08-09 00:11:15 -0300

    Criado fun Adicionar Produto

--- src/main/kotlin/CarrinhoCompras/Carrinho.kt
+++ src/main/kotlin/CarrinhoCompras/Carrinho.kt
@@ -1,9 +1,17 @@
 package CarrinhoCompras
 
 class Carrinho() {
+
+    //var produtos: ArrayList<Produto>
     /*var produtos
         get() = listOf<Produto>()
         set(value) = TODO()
 */
-    var produtos = listOf<Produto>()
+    var produtos = ArrayList<Produto>()
+    //private var contador = 0
+    fun adicionar(produto: Produto){
+        //contador =
+        produtos[produtos.size] = produto
+    }
+
 }
