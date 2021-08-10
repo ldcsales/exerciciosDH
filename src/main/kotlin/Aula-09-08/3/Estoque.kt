5b90f87432294051533257eae6b29fdee69e0642
Lucas Sales <lucas.gol@uol.com.br>
2021-08-09 23:18:17 -0300

    Iniciado Exercicio 3 Aula 09-08, criado classe Estoque e suas functions

---
+++ src/main/kotlin/Aula-09-08/3/Estoque.kt
@@ -0,0 +1,33 @@
+package `Aula-09-08`.`3`
+
+class Estoque(
+    var nome: String,
+    var qtdAtual: Int,
+    var qtdMinima: Int
+) {
+
+    fun mudarNome(nome:String){
+
+    }
+
+    fun mudarQtdMinima(qtdMinima: Int){
+
+    }
+
+    fun repor(qtd: Int){
+
+    }
+
+    fun darBaixa(qtd: Int){
+
+    }
+
+    fun mostra(){
+
+    }
+
+    fun precisaRepor(): Boolean{
+
+    }
+
+}
