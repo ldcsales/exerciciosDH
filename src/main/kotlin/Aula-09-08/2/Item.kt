18692264635a666cb919a05b3b1c96ae351a62c3
Lucas Sales <lucas.gol@uol.com.br>
2021-08-09 21:46:51 -0300

    Criado validacao de variavel no construtor

--- src/main/kotlin/Aula-09-08/2/Item.kt
+++ src/main/kotlin/Aula-09-08/2/Item.kt
@@ -6,5 +6,14 @@ class Item(
     var quantidade: Int = 0,
     var preco: Double = 0.0
 )
+{
+    init {
+        if(quantidade<0){
+            quantidade = 0
+        }
+        if(preco<0.0){
+            preco = 0.0
+        }
 
-
+    }
+}
--- src/main/kotlin/Aula-09-08/2/Run.kt
+++ src/main/kotlin/Aula-09-08/2/Run.kt
@@ -1,9 +1,9 @@
 package `Aula-09-08`.`2`
 
 fun main() {
-    var item1 = Item(1,"Calca",5,10.0)
-    var item2 = Item(2,"Camiseta",1,5.0)
-    var item3 = Item(3,"Sapato",10,20.0)
+    var item1 = Item(1, "Calca", 5, 10.0)
+    var item2 = Item(2, "Camiseta", 1, 5.0)
+    var item3 = Item(3, "Sapato", 10, 20.0)
 
     var fatura = Fatura()
     fatura.adicionarItem(item1)
