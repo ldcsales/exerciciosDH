f20780db6f6f830f927ad2c7189a0db7149e5036
Lucas Sales <lucas.gol@uol.com.br>
2021-08-18 21:40:00 -0300

    Metodo equals sobrescrito

--- src/main/kotlin/Aula-18-08/1/Pessoa.kt
+++ src/main/kotlin/Aula-18-08/1/Pessoa.kt
@@ -1,9 +1,15 @@
 package `Aula-18-08`.`1`
 
+import classes.Aluno
+
 class Pessoa(
     var nome: String,
     var rg: Int
 ) {
+
+    override fun equals(other: Any?): Boolean {
+        return (other is Pessoa && other.rg == this.rg)
+    }
 
 
 }
---
+++ src/main/kotlin/Aula-18-08/1/Run.kt
@@ -0,0 +1,16 @@
+package `Aula-18-08`.`1`
+
+fun main() {
+    var pessoa1 = Pessoa("Lucas",123)
+    var pessoa2 = Pessoa("Marcio",123)
+
+    if(pessoa1 == pessoa2){
+        println("True")
+    }else{
+        println("False")
+    }
+
+
+
+
+}
