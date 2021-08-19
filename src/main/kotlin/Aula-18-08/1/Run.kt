75f59aa4a0e944ba29fcd5312fc77f89723b927a
Lucas Sales <lucas.gol@uol.com.br>
2021-08-18 21:44:33 -0300

    Codigo identado

--- src/main/kotlin/Aula-18-08/1/Run.kt
+++ src/main/kotlin/Aula-18-08/1/Run.kt
@@ -1,19 +1,17 @@
 package `Aula-18-08`.`1`
 
 fun main() {
-    var pessoa1 = Pessoa("Lucas",123)
-    var pessoa2 = Pessoa("Marcio",123)
+    var pessoa1 = Pessoa("Lucas", 123)
+    var pessoa2 = Pessoa("Marcio", 123)
 
-    if(pessoa1 == pessoa2){
+    if (pessoa1 == pessoa2) {
         println("True")
-    }else{
+    } else {
         println("False")
     }
 //nao e necessario chamar o toString()
     println(pessoa1.toString())
     println(pessoa1.hashCode())
-
-
 
 
 }
